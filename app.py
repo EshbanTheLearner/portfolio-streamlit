b82813824dc21001afb99a9a11847fa096618a49
Eshban Suleman <eshban9492@gmail.com>
2023-01-14 14:53:59 +0500

    First commit

---
+++ app.py
@@ -0,0 +1,86 @@
+import streamlit as st 
+import streamlit.components.v1 as components
+import static
+
+st.set_page_config(page_title="Eshban Suleman", layout="wide", page_icon="👨‍🔬")
+
+with st.sidebar:
+    components.html(static.embeds["linkedin"], height=310)
+
+st.header("About Me 📖")
+st.write(static.info["intro"])
+
+st.sidebar.caption("Contact me at")
+st.sidebar.write(f"📧 {static.info['email']}")
+pdfFileObj = open(static.info["resume"], 'rb')
+st.sidebar.download_button("Download Resume", pdfFileObj, file_name="CV_Eshban.pdf", mime="pdf")
+
+st.header("Experience ⏳")
+st.subheader("Senior Data Scientist")
+st.caption("Foretheta (November 2019 - Present)")
+tab1, tab2 = st.tabs(["Responsibilities", "Tech Stack"])
+with tab1:
+        for r in static.responsibilities:
+            st.markdown(r)
+with tab2:
+        for s in static.stack:
+            st.markdown(s)
+
+st.header("Skills & Tools ⚒️")
+levels = ["Advance", "Intermediate", "Beginner"]
+for tab, level in zip(st.tabs(levels), levels):
+    with tab:
+        for i, j, k in zip(static.skills[level][::3], static.skills[level][1::3], static.skills[level][2::3]):
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.markdown(i)
+            with col2:
+                st.markdown(j)
+            with col3:
+                st.markdown(k)
+
+
+st.header("Projects 🗃")
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Reginald von Doom 🤵🏽")
+    with st.expander("Info"):
+        for item in static.projects_info["Reginald von Doom"]:
+            st.markdown(item)
+    with st.expander("Reginald von Doom Quick Demo"):
+        video_file = open(static.vids["reginald-vid"], "rb")
+        video_bytes = video_file.read()
+        st.video(video_bytes)
+
+with col2:
+    st.subheader("Movie Recommendation Engine 🎥")
+    with st.expander("Info"):
+        for item in static.projects_info["MRE"]:
+            st.markdown(item)
+    with st.expander("Movie Recommendation Engine Quick Demo"):
+        video_file = open(static.vids["mre-vid"], "rb")
+        video_bytes = video_file.read()
+        st.video(video_bytes)
+
+st.header("Certifications 🗂")
+counter = 0
+with st.container():
+    with st.expander("View My Certificates Below"):
+        for row in range(2):
+            for col in st.columns(3):
+                with col:
+                    col.image(static.imgs[counter])
+                    st.subheader(static.imgs_titles[counter])
+                    st.markdown(static.imgs_captions[counter], unsafe_allow_html=True)
+                    counter += 1
+        st.markdown(f"To see more, [click here]({static.link})", unsafe_allow_html=True)
+
+st.header("Blogs ✍🏽")
+with st.expander("Read My Blogs Below"):
+    st.markdown("""<a href={}> Access Full Profile Here</a>""".format(static.info["medium"]), unsafe_allow_html=True)
+    components.html(static.embeds["medium"], height=500)
+
+st.header("Twitter 🔔")
+with st.expander("Follow Me On Twitter"):
+    components.html(static.embeds["twitter-feed"], height=500)
+    components.html(static.embeds["twitter-profile"], height=100)
---
+++ static.py
@@ -0,0 +1,116 @@
+info = {
+    "name": "Eshban Suleman",
+    "intro": """I'm a Data Scientist with 3+ years of broad-based experience in building data-intensive applications, and overcoming complex architectural issues. Proficient in predictive modelling, large-scale data processing, NLP and statistical analysis, as well as scripting languages like Python. Capable of creating, developing, testing, and deploying highly adaptive diverse services to translate business and functional qualifications into substantial deliverables.
+                \nI like to read, mostly non-fiction, self-help books. I enjoy wathcing movies and TV shows, my favorite generes are fantasy, sci-fi, psychological thrillers, adventure. I enjoy sports as well. I love to play and watch cricket, table tennis, basketball, chess. I like to spend my free time by learning about something new/interesting. And that's pretty much about me! 😄""",
+    "photo": {"path": "photo.jpg", "width": 150},
+    "mobile": "+92-304-4455675",
+    "email": "eshban9492@gmail.com",
+    "resume": "pdf/CV_Eshban.pdf",
+    "medium": "https://medium.com/@eshban9492",
+    "publication_url": "https://eshban9492.medium.com/",
+    "skills": [
+        "Exploratory Data Analysis", "Feature Engineering", "Data Analysis",
+        "Predictive Modeling", "Data Visualization", "Machine Learning",
+        "Deep Learning", "Natural Language Processing", "Time Series Analysis",
+        "A/B Testing", "Web APIs (Flask, FastAPI)", "SQL (BigQuery, PostgreSQL)", 
+        "Git", "Streamlit",
+        "MLOps (DVC, MLFlow, Docker, GitHub Actions etc.)",
+        "Python (Pandas, Numpy, Scikit-Learn, Tensorflow, PyTorch etc.)",
+        "AWS (S3, EC2, Lambda, Sagemaker, API Gateway etc.)",
+    ]
+}
+
+responsibilities = [
+    """- **Lead the research and development** of a semantic search engine""",
+    """- **Developed tests and benchmarks** to evaluate the performance of our ML models""",
+    """- **Improved the performance** of our production ML model by **49%** in one case and by **6%** in another by **improving data quality** and **hyperparameter optimization**""",
+    """- **Decreased the latency** of our API by **40%** by optimizing the architecture for inference time""",
+    """- **Reduced costs** of our APIs by **72%** in one case and **68%** in another by developing an **automated self-replacing spot servers system** in AWS""",
+    """- **Brought deployment time down** by **83%** by implementing a robust **CI/CD pipeline** using **DVC, Docker, GitHub Actions and AWS Elastic Beanstalk**""",
+    """- **Designed and developed data pipelines** to fetch data and update our online ML models""",
+    """- **Took additional responsibilities** such as **interviewing, onboarding, mentoring, planning product roadmaps, delegating tasks** etc."""
+]
+
+stack = [
+    """- **Python** (Numpy, Pandas, Flask, Gensim, Transformers, FAISS, Tensorflow, PyTorch)""",
+    """- **Database** (PostgreSQL, BigQuery, Redis)""",
+    """- **MLOps** (Git, DVC, Docker, GitHub Actions, AWS Elastic Beanstalk)""",
+    """- **AWS** (Sagemaker, S3, EC2, Lambda, API Gateway etc.)"""
+]
+
+skills = {
+    "Advance": [
+        """- Python Programming (Numpy, Pandas)""", """- WebAPIs (Flask, FastAPI)""", """- SQL (PostgreSQL, BigQuery)""",
+        """- Exploratory Data Analysis""", """- Feature Engineering""", """- Preditive Modeling""",
+        """- Machine Learning (Scikit-Learn, Statsmodel)""", """- Deep Learning (Tensorflow, Keras, PyTorch)""", """- Natural Language Processing (NLTK, SpaCy, Transformers)""",
+        """- Data Visualization (Matplotlib, Seaborn, Plotly, Streamlit)""", """- AWS (Sagemaker, S3, EC2, Lambda etc.)""", """- MLOps (Git, DVC, Docker, GitHub Actions etc)"""
+    ],
+    "Intermediate": [
+        """- Time Series Analysis""", """- A/B Testing""", """- Risk Minimization""",
+        """- Computer Vision (Detection, Classification, Segmentation etc)""", """- Apache Spark""", """- GCP (Storage, CE, GKE, BigQuery etc.)""",
+        """""", """""", """""",
+        """""", """""", """""",
+    ],
+    "Beginner": [
+        """- Apache Aiflow""", """- Apache Kafka""", """- Causal Inference""",
+        """- Graph Analysis""", """""", """""",
+        """""", """""", """""",
+        """""", """""", """""",
+    ]
+}
+
+projects_info = {
+    "Reginald von Doom": [
+        """- Reginald von Doom is a system that summarizes legal or financial documents in an easy to understand form""",
+        """- Used Google's T5 model to summarize documents""",
+        """- Created a simple UI in streamlit""",
+        """- Deployed to Streamlit cloud"""
+    ],
+    "MRE": [
+        """- It is a simple movie recommendation engine""",
+        """- Analyzed the TMDB data and created features in form of tags (description, cast, crew)""",
+        """- Used Sentence Transformer model to extract feature embeddings and cosine similarity to find similar content""",
+        """- Created a simple UI in Streamlit and deployed the app to the cloud"""
+    ]
+}
+
+embeds = {
+    "linkedin": """<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="eshban-suleman-624a49113" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pk.linkedin.com/in/eshban-suleman-624a49113?trk=profile-badge"></a></div>""",
+    "twitter-profile": """<a href="https://twitter.com/EshbanSuleman?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @EshbanSuleman</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>""",
+    "twitter-feed": """<a class="twitter-timeline" href="https://twitter.com/EshbanSuleman?ref_src=twsrc%5Etfw">Tweets by EshbanSuleman</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>""",
+    "medium": """<div style="overflow-y: scroll; height:500px;"> <div id="retainable-rss-embed" data-rss="https://medium.com/feed/@eshban9492" data-maxcols="3" data-layout="grid" data-poststyle="inline" data-readmore="Read the rest" data-buttonclass="btn btn-primary" data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+vids = {
+    "mre-vid": "./media/vids/mre.mp4",
+    "reginald-vid": "./media/vids/reginald.mp4"
+}
+
+imgs = [
+    "./media/imgs/1.jpg",
+    "./media/imgs/2.jpg",
+    "./media/imgs/3.jpg",
+    "./media/imgs/4.jpg",
+    "./media/imgs/5.jpg",
+    "./media/imgs/6.jpg",
+]
+
+imgs_titles = [
+    "Data Science Professional",
+    "Data Engineering, Big Data, and Machine Learning on GCP",
+    "Applied Data Science",
+    "Mathematics for Machine Learning",
+    "Tensorflow in Practice",
+    "Deep Learning"
+]
+
+imgs_captions = [
+    "Databricks Academy ([Verify](https://academy.databricks.com/award/completion/35e13f64-01d3-3c38-93ec-146c978ba2fe/view-ext))",
+    "Google Cloud ([Verify](https://www.coursera.org/account/accomplishments/specialization/certificate/BTX3N9JPQ5AW))",
+    "IBM ([Verify](https://www.coursera.org/account/accomplishments/specialization/FRZJAJNXW6UN))",
+    "Imperial College London ([Verify](https://www.coursera.org/account/accomplishments/specialization/JLNS4URRRENR))",
+    "Deeplearning.ai ([Verify](https://www.coursera.org/account/accomplishments/specialization/MCZZWYTTM3QP))",
+    "Deeplearning.ai ([Verify](https://www.coursera.org/account/accomplishments/specialization/QCNQP6JC97Q9))"
+]
+
+link = "https://github.com/EshbanTheLearner/Certifications"
